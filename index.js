1150b2af87c09ea508894b3293828bad71a71272
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2024-05-02 11:53:36 +0300

    solution task

--- index.js
+++ index.js
@@ -3,8 +3,31 @@ const xor = (a, b) => (a === b ? false : true);
 xor(true, true);
 
 // Task 2
+const hello = (name) =>
+  name
+    ? `Hello, ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`
+    : `Hello, World!`;
+console.log(hello("joHn"));
 // Task 3
+const factorial = (n) => {
+  if (n < 0 || n > 12) {
+    throw new RangeError("Invalid number");
+  }
+  if (n <= 1) {
+    return 1;
+  }
+  return factorial(n - 1) * n;
+};
+factorial(13);
 // Task 4
+const findDifference = (a, b) =>
+  Math.abs(
+    [a.reduce((a, b) => a * b), b.reduce((a, b) => a * b)].reduce(
+      (a, b) => a - b
+    )
+  );
+
+findDifference([10, 2, 20], [25, 19, 27]);
 // Task 5
 // Task 6
 // Task 7
